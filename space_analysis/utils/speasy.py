e28347687bcac799af73c6175c90d252ec19a96e
Beforerr <zzj956959688@gmail.com>
2024-06-04 14:07:27 -0700

    clean notebooks

--- space_analysis/utils/speasy.py
+++ space_analysis/utils/speasy.py
@@ -2,13 +2,15 @@
 
 # %% auto 0
 __all__ = ['spzvar2pldf', 'spzvars2pldf', 'get_time_resolution', 'get_provider', 'get_dataset_index', 'get_dataset_parameters',
-           'get_parameter_index', 'Variable', 'Variables']
+           'get_parameter_index', 'Variable', 'Variables', 'SVariables']
 
-# %% ../../nbs/utils/19_speasy.ipynb 0
+# %% ../../nbs/utils/19_speasy.ipynb 1
 import speasy as spz
 import polars as pl
 
-from pydantic import model_validator
+from fastcore.all import patch
+from pydantic import model_validator, ConfigDict
+from pydantic.dataclasses import dataclass
 from functools import cached_property
 from ..core import Variables as Vs
 from ..core import Variable as V
@@ -20,7 +22,7 @@ from speasy.core.inventory import DatasetIndex, ParameterIndex
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import Axes
 
-# %% ../../nbs/utils/19_speasy.ipynb 1
+# %% ../../nbs/utils/19_speasy.ipynb 3
 def spzvar2pldf(var: SpeasyVariable):
     # see SpeasyVariable.to_dataframe
     var = var.replace_fillval_by_nan()
@@ -37,11 +39,11 @@ def spzvars2pldf(vars: list[SpeasyVariable]):
         return spzvar2pldf(vars[0])
     return pl.concat([spzvar2pldf(var) for var in vars], how="align")
 
-# %% ../../nbs/utils/19_speasy.ipynb 2
+# %% ../../nbs/utils/19_speasy.ipynb 4
 def get_time_resolution(data: SpeasyVariable):
     return pl.Series(data.time).diff().describe()
 
-# %% ../../nbs/utils/19_speasy.ipynb 3
+# %% ../../nbs/utils/19_speasy.ipynb 5
 def get_provider(v: str) -> DataProvider:
     if v == "cda":
         return spz.cda
@@ -63,34 +65,86 @@ def get_parameter_index(param: str, ds: str) -> ParameterIndex:
     ds_info = vars(get_dataset_index(ds))
     return ds_info[param]
 
-# %% ../../nbs/utils/19_speasy.ipynb 4
-class Variable(V):
+# %% ../../nbs/utils/19_speasy.ipynb 6
+from humanize import naturalsize
 
-    def to_polars(self):
-        return spzvar2pldf(self.data)
+# %% ../../nbs/utils/19_speasy.ipynb 7
+@patch
+def preview(self: SpeasyVariable):
+    print("===========================================")
+    print(f"Name:         {self.name}")
+    print(f"Columns:      {self.columns}")
+    print(f"Values Unit:  {self.unit}")
+    print(f"Memory usage: {naturalsize(self.nbytes)}")
+    print(f"Axes Labels:  {self.axes_labels}")
+    print("-------------------------------------------")
+    print(f"Meta-data:    {self.meta}")
+    print("-------------------------------------------")
+    print(f"Time Axis:    {self.time[:3]}")
+    print("-------------------------------------------")
+    print(f"Values:       {self.values[:3]}")
+    print("===========================================")
+
+# %% ../../nbs/utils/19_speasy.ipynb 9
+class Variable(V):
+    parameter: str = None
+    product: str = None
+    """product name should be unique"""
 
     @cached_property
     def data(self) -> SpeasyVariable:
         return spz.get_data(self.product, self.timerange)
-
+    
     @property
     def time_resolution(self):
         return get_time_resolution(self.data)
+    
+    def to_polars(self):
+        return spzvar2pldf(self.data)
 
-    def plot(self, fig=None, ax: Axes = None):
-        if fig is None and ax is None:
-            fig, ax = plt.subplots()
+    def preview(self):
+        return self.to_polars().head().collect()
 
-        self.data.replace_fillval_by_nan().plot(ax=ax)
+    @classmethod
+    def load_from_file(cls, path: str):
+        """Load the configuration from a file."""
+        import yaml
 
-        if self.name:
-            ax.set_ylabel(self.name)
+        with open(path, "r") as f:
+            yy = yaml.load(f, Loader=yaml.FullLoader)
+            return cls(**yy)
 
-        return fig, ax
+# %% ../../nbs/utils/19_speasy.ipynb 10
+@patch
+def plot(self: Variable, fig=None, ax: Axes = None):
+    if fig is None and ax is None:
+        fig, ax = plt.subplots()
 
-# %% ../../nbs/utils/19_speasy.ipynb 5
+    self.data.replace_fillval_by_nan().plot(ax=ax)
+
+    if self.name:
+        ax.set_ylabel(self.name)
+
+    return fig, ax
+
+@patch
+def dump(self: Variable, path: str):
+    """Dump the configuration to a file."""
+    import yaml
+
+    with open(path, "w") as f:
+        yy = yaml.load(
+            self.model_dump_json(exclude_defaults=True), Loader=yaml.FullLoader
+        )
+        yaml.dump(yy, f)
+
+# %% ../../nbs/utils/19_speasy.ipynb 11
+@dataclass(config=ConfigDict(arbitrary_types_allowed=True))
 class Variables(Vs):
+    parameters: list[str] = None
     variables: list[Variable] = None
+    dataset: str = None
+    provider: str = "cda"
     products: list[str | ParameterIndex] = None
 
     # initize products from provider and dataset if not provided
@@ -137,3 +191,10 @@ class Variables(Vs):
             var.plot(ax=ax)
 
         return fig, axes
+    
+class SVariables:
+
+    products: list[str] = None
+    parameters: list[str] = None
+
+    _data: list[Variable] = None
