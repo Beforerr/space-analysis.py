ad401b1e61f1dfd3fd1594a73990756850ec6a34
Beforerr <zzj956959688@gmail.com>
2024-11-11 10:16:39 -0800

    fix: convert when not giving cols

--- src/space_analysis/ds/ts/io.py
+++ src/space_analysis/ds/ts/io.py
@@ -7,9 +7,10 @@ __all__ = ['df2ts']
 from xarray import DataArray
 import polars as pl
 import pandas as pd
+import numpy as np
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 1
-def create_data_array(data, time, cols, **kwargs):
+def create_data_array(data: np.ndarray, time, cols: list | str, **kwargs):
     if isinstance(cols, str) and len(data.shape) > 1:
         element_len = data.shape[1]
         v_dim = [cols + str(i) for i in range(element_len)]
@@ -20,33 +21,38 @@ def create_data_array(data, time, cols, **kwargs):
     return DataArray(data, coords=coords, **kwargs)
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 2
-def pddf2ts(df: pd.DataFrame, cols=None, **kwargs):
+def pddf2ts(df: pd.DataFrame, cols: list | str = None, **kwargs):
+    cols = cols or df.columns.tolist()
     data = df[cols].to_numpy()
     time = df.index
-    cols = cols or df.columns.tolist()
     return create_data_array(data, time, cols, **kwargs)
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 3
+def get_columns(df: pl.DataFrame | pl.LazyFrame):
+    if isinstance(df, pl.LazyFrame):
+        return df.collect_schema().names()
+    return df.columns
+
+
 def pldf2ts(
     df: pl.DataFrame | pl.LazyFrame,
-    cols=None,
+    cols: list | str = None,
     time_col="time",
     **kwargs,
 ):
-    if isinstance(df, pl.LazyFrame):
-        df = df.collect()
-
-    data = df[cols].to_numpy()
-    time = df[time_col]
-    cols = cols or df.columns
+    cols = cols or get_columns(df)
     if time_col in cols:
         cols.remove(time_col)
+    if isinstance(df, pl.LazyFrame):
+        df = df.collect()
+    data = df[cols].to_numpy()
+    time = df[time_col]
     return create_data_array(data, time, cols, **kwargs)
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 4
 def df2ts(
     df,
-    cols=None,
+    cols: list | str = None,
     **kwargs,
 ):
     """Convert DataFrame to TimeSeries"""
