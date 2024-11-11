9c0e9c8991f4aab308437fac25da63566828ae33
Beforerr <zzj956959688@gmail.com>
2024-11-11 12:01:50 -0800

    refactor: df2ts add time keyword and convert into ns for xarray

--- src/space_analysis/ds/ts/io.py
+++ src/space_analysis/ds/ts/io.py
@@ -11,21 +11,28 @@ import numpy as np
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 1
 def create_data_array(data: np.ndarray, time, cols: list | str, **kwargs):
-    if isinstance(cols, str) and len(data.shape) > 1:
-        element_len = data.shape[1]
-        v_dim = [cols + str(i) for i in range(element_len)]
-    else:
-        v_dim = cols
+    # convert time from non-nanoseconds into nanoseconds precision
+    # https://github.com/pydata/xarray/issues/7493
+    if time.dtype != "datetime64[ns]":
+        time = time.astype("datetime64[ns]")
 
-    coords = {"time": time, "v_dim": v_dim}
+    if len(data.shape) == 1:
+        coords = {"time": time}
+    else:
+        if isinstance(cols, str) and data.shape[1] > 1:
+            v_dim = [f"{cols}_{i}" for i in range(data.shape[1])]
+        else:
+            v_dim = cols
+        coords = {"time": time, "v_dim": v_dim}
+
     return DataArray(data, coords=coords, **kwargs)
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 2
-def pddf2ts(df: pd.DataFrame, cols: list | str = None, **kwargs):
+def pddf2ts(df: pd.DataFrame, cols: list | str = None, time=None, **kwargs):
     cols = cols or df.columns.tolist()
     data = df[cols].to_numpy()
-    time = df.index
-    return create_data_array(data, time, cols, **kwargs)
+    times = df.index if time is None else df[time]
+    return create_data_array(data, times, cols, **kwargs)
 
 # %% ../../../../nbs/data_structure/timeseries/io.ipynb 3
 def get_columns(df: pl.DataFrame | pl.LazyFrame):
@@ -37,26 +44,27 @@ def get_columns(df: pl.DataFrame | pl.LazyFrame):
 def pldf2ts(
     df: pl.DataFrame | pl.LazyFrame,
     cols: list | str = None,
-    time_col="time",
+    time="time",
     **kwargs,
 ):
     cols = cols or get_columns(df)
-    if time_col in cols:
-        cols.remove(time_col)
+    if time in cols:
+        cols.remove(time)
     if isinstance(df, pl.LazyFrame):
         df = df.collect()
     data = df[cols].to_numpy()
-    time = df[time_col]
-    return create_data_array(data, time, cols, **kwargs)
+    times = df[time].to_numpy()
+    return create_data_array(data, times, cols, **kwargs)
 
-# %% ../../../../nbs/data_structure/timeseries/io.ipynb 4
+# %% ../../../../nbs/data_structure/timeseries/io.ipynb 5
 def df2ts(
     df,
     cols: list | str = None,
+    time: str = "time",
     **kwargs,
 ):
     """Convert DataFrame to TimeSeries"""
     if isinstance(df, (pl.DataFrame, pl.LazyFrame)):
-        return pldf2ts(df, cols, **kwargs)
+        return pldf2ts(df, cols, time, **kwargs)
     elif isinstance(df, pd.DataFrame):
-        return pddf2ts(df, cols, **kwargs)
+        return pddf2ts(df, cols, time, **kwargs)
